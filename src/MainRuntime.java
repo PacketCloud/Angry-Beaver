0f93baecfb15ab988dbbf67630120042b6aa60ad
Ewong3 <eric3@ualberta.ca>
2016-07-15 18:56:31 -0600

    MainRuntime.class opens a window

--- src/MainRuntime.java
+++ src/MainRuntime.java
@@ -1,8 +1,12 @@
+import javax.swing.JFrame;
 
 public class MainRuntime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setSize(1920, 1080);
+		frame.setVisible(true);
 	}
 
 }
