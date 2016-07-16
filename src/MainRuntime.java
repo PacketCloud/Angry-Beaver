490572a3bc6866b65412501e334ecbd8b3508abe
Ewong3 <eric3@ualberta.ca>
2016-07-15 19:18:21 -0600

    MainRuntime.class adjusted window size

--- src/MainRuntime.java
+++ src/MainRuntime.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JFrame;
 
 public class MainRuntime {
@@ -5,8 +7,15 @@ public class MainRuntime {
 	public static void main(String[] args) {
 		// Opens a 1920x1080 window to start the game
 		JFrame frame = new JFrame();
-		frame.setSize(1920, 1080);
+		frame.setSize(1280, 720);
 		frame.setVisible(true);
+		/*
+		Graphics graphics = frame.getGraphics();
+		graphics.setColor(Color.BLUE);
+		graphics.drawRect(0, 0, 100, 100);
+		graphics.fillRect(0, 0, 100, 100);
+		frame.update(graphics);
+		*/
 	}
 
 }
