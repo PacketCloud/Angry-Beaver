30233d9f8d5292f166aff9f9915a77e6d2761661
aschuldhaus <adrianschuldhaus@gmail.com>
2016-09-29 13:11:22 -0600

    Added ResourceHandling package, with ResourceCollection and ResourceState classes.
    ResourceState will be used in ResourceCollection. Entity classes will
    use a single ResourceCollection to associate assets (sprites and sounds)
    with any given state for such entity. As well as information on how
    (where and when) to use them, in respect to hitbox (for visual assets),
    and time/state-change (for audio).

---
+++ src/ResourceHandling/ResourceCollection.java
@@ -0,0 +1,9 @@
+package ResourceHandling;
+
+import ResourceState;
+
+public class ResourceCollection {
+//A structure referencing all assets associated with an entity for all possible states.
+//This way the game knows what to render for each entity, or what sounds to play, depending on what state it is in.
+//Entity classes should be reworked first, with this package in mind.
+}
---
+++ src/ResourceHandling/ResourceState.java
@@ -0,0 +1,8 @@
+package ResourceHandling;
+
+import javax.swing.ImageIcon;
+import java.util.ArrayList;
+
+public class ResourceState {
+	//Simple type, associating an entity state with the resource(s) to render while in that state, how to render them, and any sounds to play (and how/when to play them).
+}
