bb3c696fa63a0a4706fa51feeea2fd5b860bd2b7
aschuldhaus <adrianschuldhaus@gmail.com>
2016-10-12 14:27:40 -0600

    Changed Behaviour class/interface structure.
    Switched the Behaviour interface with BehaviourInterface, making
    Behaviour a class.
    Entity-specific behaviour classes will implement the BehaviourInterface.
    Behaviour will then call to the static methods in the specific behaviour
    classes corresponding with the type of entity associated to the call.
    This will allow each aspect of an entity to be defined separately:
    visual assets, audio assets, behaviour, combat stats, etc.

---
+++ src/entities/Behaviour.java
@@ -0,0 +1,10 @@
+package entities;
+
+import entities.MooseBehaviour;
+//import entities.LumberjackBehaviour;
+//import entities.BearBehaviour;
+
+public class Behaviour {
+//Will be used in entity (or state) to call correct behaviour methods depending on entity type
+	
+}
--- src/entities/BehaviourInterface.java
+++ src/entities/BehaviourInterface.java
@@ -1,6 +1,6 @@
 package entities;
 
-public interface Behavior {
+public interface BehaviourInterface {
 	//Empty definition for now, so implementing classes do not complain.
 	
 	//TODO: Define Behavior interface, then implement in subclasses.
