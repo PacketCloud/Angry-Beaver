d33b9b1326c674f4cfb3e2eab28ca8091e23b810
alosious <alosious@ualberta.ca>
2016-11-11 15:42:51 -0700

    State Menu

--- src/states/gameState/GameStateMenu.java
+++ src/states/gameState/GameStateMenu.java
@@ -1,5 +1,26 @@
 package states.gameState;
 
-public class GameStateMenu {
+import java.awt.Graphics2D;
+
+import engine.LevelHandler;
+
+public class GameStateMenu extends GameStateAbstract {
+
+	public GameStateMenu(LevelHandler h) {
+		super(h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
