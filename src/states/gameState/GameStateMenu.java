cf11d6c0cf9efbdf97271c84eb90c188357fc73c
alosious <alosious@ualberta.ca>
2016-11-11 18:47:41 -0700

    test

--- src/states/gameState/GameStateMenu.java
+++ src/states/gameState/GameStateMenu.java
@@ -1,5 +1,6 @@
 package states.gameState;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.IOException;
@@ -31,13 +32,17 @@ public class GameStateMenu extends GameStateAbstract {
 		OpenImage Oimage = new OpenImage();
 		
 		try {
-			g.scale(.5, .5);
-
+			Font font = new Font("Comic Sans MS", 0, 50);
+			g.setFont(font);
 			g.drawImage(Oimage.OpenImage(""), 0,0, null);
+			g.drawString("ANGRY BEAVER", 500, 100);
+			g.drawString("PLAY", 625, 200);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	
 
 }
