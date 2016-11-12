52d469a604df6c0c76aa5ce5f600b1fceb096162
alosious <alosious@ualberta.ca>
2016-11-11 21:19:26 -0700

    added some feature to menu
    menu selection is working but hightlight still not working

--- src/states/gameState/GameStateMenu.java
+++ src/states/gameState/GameStateMenu.java
@@ -4,25 +4,30 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
+import javax.swing.KeyStroke;
 
 import FileUtility.OpenImage;
 import engine.LevelHandler;
 
 public class GameStateMenu extends GameStateAbstract {
-
+	public ArrayList<String> strings = new ArrayList<String>(Arrays.asList("Play", "Options","EXIT"));
+	public int chosen = 0;	
+	
 	public GameStateMenu(LevelHandler h) {
 		super(h);
-		
-		
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+		getH().repaint();
+    	System.out.println("Paused");
 	}
 
 	@Override
@@ -32,16 +37,73 @@ public class GameStateMenu extends GameStateAbstract {
 		OpenImage Oimage = new OpenImage();
 		
 		try {
-			Font font = new Font("Comic Sans MS", 0, 50);
+			Font font = new Font("Arial", 0, 100);
 			g.setFont(font);
 			g.drawImage(Oimage.OpenImage(""), 0,0, null);
-			g.drawString("ANGRY BEAVER", 500, 100);
-			g.drawString("PLAY", 625, 200);
+			
+			g.drawString("ANGRY BEAVER", 290, 100);
+			Font font1 = new Font("Comic Sans MS", 0, 50);
+			g.setFont(font1);
+			g.drawString("PLAY", 600, 300);
+			g.drawString("OPTIONS", 550, 400);
+			g.drawString("EXIT", 600, 500);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	
+	//TODO:  figure out how to hightlight options in menu
+	//key up down is working 
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+		super.jump();
+		switch(chosen) {
+		case 0:
+			getH().getGameState().stateRun();
+			break;
+		case 1:
+			break;
+		case 2:
+			getH().getGameState().stateStop();
+		}
+	}
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+		super.up();
+		if( chosen > 0){
+			chosen = chosen -1;
+			System.out.print(chosen);
+
+		}
+		if(chosen < 0){
+			chosen = 2;
+			System.out.print(chosen);
+
+		}
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+		super.down();
+		if( chosen >= 0){
+			chosen = chosen +1;
+			System.out.print(chosen);
+
+		}
+		if(chosen > 2 ){
+			chosen = 0;
+			System.out.print(chosen);
+
+		}	}
+	
+	
 	
 	
 
